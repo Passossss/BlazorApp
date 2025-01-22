7b8dae07e5753b202f2a162f42e54b34b3376395
Gustavo Passos <gusapas26@gmail.com>
2025-01-22 14:28:05 -0300

    first

--- obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d744c3422301bee022821cc993179d5eea4da830")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c7cda147b114d287cf71a1febad96c268851979")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
